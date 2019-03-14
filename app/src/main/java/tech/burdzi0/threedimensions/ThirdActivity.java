7b84200a95bc7dbcf684c67ff6a09d7149035b87
Burdzi0 <burdzi0@programmer.net>
2019-03-14 08:18:34 +0100

    Add color update on seek bar change. Change app name in Manifest. Update apk file

--- app/src/main/java/tech/burdzi0/threedimensions/ThirdActivity.java
+++ app/src/main/java/tech/burdzi0/threedimensions/ThirdActivity.java
@@ -1,25 +1,19 @@
 package tech.burdzi0.threedimensions;
 
-import android.graphics.Color;
-import android.support.annotation.ColorInt;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ProgressBar;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.Toast;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.function.Consumer;
 
 public class ThirdActivity extends AppCompatActivity {
 
     private RadioGroup radioGroup;
     private View view;
     private SeekBar seekBar;
+    private int currentColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +26,24 @@ public class ThirdActivity extends AppCompatActivity {
 
         seekBar.setMax(255);
         seekBar.setMin(0);
+        seekBar.setProgress(100);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                view.setBackgroundColor(getColorWithOpacity(currentColor));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         view = findViewById(R.id.view3);
 
@@ -40,20 +52,29 @@ public class ThirdActivity extends AppCompatActivity {
             switch (checkedId) {
                 case R.id.radioButton:
                     color = getColorWithOpacity(R.color.colorRed);
+                    currentColor = R.color.colorRed;
+                    showToast("You chose red!");
                     break;
                 case R.id.radioButton2:
                     color = getColorWithOpacity(R.color.colorBlue);
+                    currentColor = R.color.colorBlue;
+                    showToast("You chose blue!");
                     break;
                 case R.id.radioButton3:
                     color = getColorWithOpacity(R.color.colorGreen);
+                    currentColor = R.color.colorGreen;
+                    showToast("You chose green!");
                     break;
             }
             view.setBackgroundColor(color);
         });
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
+
+        view.setBackgroundColor(getColorWithOpacity(R.color.colorRed));
     }
 
-    private int getColorWithOpacity(int p) {
-        String colorWithoutOpacity = Integer.toHexString(getResources().getColor(p));
+    private int getColorWithOpacity(int color) {
+        String colorWithoutOpacity = Integer.toHexString(getResources().getColor(color));
         String opacity = Integer.toHexString(seekBar.getProgress());
         String result = opacity + colorWithoutOpacity.substring(2);
         return Long.decode("0x" + result).intValue();
